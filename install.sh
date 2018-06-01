67ff28fff3abc8224b3ad9ea7ce96f87a7265b46
Neil Dwyer <neildwyer1991@gmail.com>
2018-06-01 22:41:09 +0000

    typos

--- install.sh
+++ install.sh
@@ -2,12 +2,8 @@ LD_LIBRARY_PATH=/usr/local/lib
 
 set -e
 
-
 ### gstreamer
-rm gstreamer-1.14.1.tar.xz
 wget https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.14.1.tar.xz
-
-rm -rf gstreamer-1.14.1
 tar xvf gstreamer-1.14.1.tar.xz
 
 cd gstreamer-1.14.1
@@ -20,64 +16,36 @@ checkinstall -D -y --install=no make install
 cd ..
 
 ## base
-rm gst-plugins-base-1.14.1.tar.xz
 wget https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-1.14.1.tar.xz
-
-rm -rf gst-plugins-base-1.14.1
-tar xvf gst-plugins-base-1.14.1
-
+tar xvf gst-plugins-base-1.14.1.tar.xz
 cd gst-plugins-base-1.14.1
-
 ./autogen.sh --enable-introspection
 make -j2
-
 checkinstall -D -y --install=no make install
-
 cd ..
 
 ## good 
-rm gst-plugins-good-1.14.1.tar.xz
 wget https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-1.14.1.tar.xz
-
-rm -rf gst-plugins-good-1.14.1
-tar xvf gst-plugins-good-1.14.1
-
+tar xvf gst-plugins-good-1.14.1.tar.xz
 cd gst-plugins-good-1.14.1
-
 ./autogen.sh --enable-introspection
 make -j2
-
 checkinstall -D -y --install=no make install
-
 cd ..
 
 ## bad 
-rm gst-plugins-bad-1.14.1.tar.xz
 wget https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-1.14.1.tar.xz
-
-rm -rf gst-plugins-bad-1.14.1
-tar xvf gst-plugins-bad-1.14.1
-
+tar xvf gst-plugins-bad-1.14.1.tar.xz
 cd gst-plugins-bad-1.14.1
-
 ./autogen.sh --enable-introspection
 make -j2
-
 checkinstall -D -y --install=no make install
-
 cd ..
 
 ## av 
-rm gst-libav-1.14.1.tar.xz
 wget https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.14.1.tar.xz
-
-rm -rf gst-libav-1.14.1
-tar xvf gst-libav-1.14.1
-
+tar xvf gst-libav-1.14.1.tar.xz
 cd gst-libav-1.14.1
-
 ./autogen.sh --enable-introspection
 make -j2
-
 checkinstall -D -y --install=no make install
-
