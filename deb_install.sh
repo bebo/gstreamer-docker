b3a6920c8902131b635e44a03912a38dc1449a8b
Neil Dwyer <neildwyer1991@gmail.com>
2018-06-04 20:41:16 +0000

    don't actually need the deb_install.sh

--- deb_install.sh
+++
@@ -1,6 +0,0 @@
-sudo dpkg -i gstreamer*.deb
-sudo dpkg -i gst-plugins-base*.deb
-sudo dpkg -i gst-plugins-good*.deb
-sudo dpkg -i gst-plugins-bad*.deb
-sudo dpkg -i gst-plugins-ugly*.deb
-sudo dpkg -i gst-libav*.deb
