5804c3d45f1f751ff60fc2960fc000237493c966
Neil Dwyer <neildwyer1991@gmail.com>
2018-06-04 20:34:46 +0000

    added deb_install.sh

---
+++ deb_install.sh
@@ -0,0 +1,6 @@
+sudo dpkg -i gstreamer*.deb
+sudo dpkg -i gst-plugins-base*.deb
+sudo dpkg -i gst-plugins-good*.deb
+sudo dpkg -i gst-plugins-bad*.deb
+sudo dpkg -i gst-plugins-ugly*.deb
+sudo dpkg -i gst-libav*.deb
