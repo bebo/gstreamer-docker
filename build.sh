852f9339f978e4580d08d27c28423c64be8c9482
Furqan Rydhan <furqan.rydhan@gmail.com>
2018-08-16 07:57:25 +0000

    build should be functional

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -x
 
 docker build -t ${JOB_NAME}:${TAG} --rm --no-cache .
 docker tag ${JOB_NAME}:${TAG} bebodev/${JOB_NAME}:${TAG}
-docker push bebodev/${JOB_NAME}:${TAG}
+docker push bebodev/${JOB_NAME}
